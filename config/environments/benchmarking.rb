09e295d7b89afe8b66ff6b7af042bb7ee96ccc93
Bryan Stearns <bryanstearns@gmail.com>
2008-09-02 16:05:24 -0700

    Add basic perf measurement - see notes at https://selfamusementpark.com/trac/wiki/RubyPerformance

---
+++ config/environments/benchmarking.rb
@@ -0,0 +1,27 @@
+# The benchmarking environment is sthe same as production, except with
+# more logging, no exception-notification, and no asset hosts.
+config.log_level = :debug
+
+config.cache_classes = true
+
+# Use a different logger for distributed setups
+# config.logger = SyslogLogger.new
+
+# Full error reports are disabled and caching is turned on
+config.action_controller.consider_all_requests_local = false
+config.action_controller.perform_caching             = true
+
+# Use a different cache store in production
+# config.cache_store = :mem_cache_store
+
+# Enable serving of images, stylesheets, and javascripts from an asset server
+# config.action_controller.asset_host                  = "http://assets.example.com"
+# BJS: added:
+# config.action_controller.asset_host                  = "http://assets%d.festivalfanatic.com"
+
+# Disable delivery errors, bad email addresses will be ignored
+# config.action_mailer.raise_delivery_errors = false
+
+# BJS: Configure exception_notification
+#ExceptionNotifier.exception_recipients = %w(festfan@festivalfanatic.com)
+#ExceptionNotifier.sender_address = %("FestivalFanatic" <festfan@festivalfanatic.com>)
