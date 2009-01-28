7b74473d9ec4cf69a929c47db184c84e3b4e6042
Bryan Stearns <bryanstearns@gmail.com>
2009-01-27 16:20:03 -0800

    determine revision in initializer; use as part of cache namespace

--- config/initializers/cache.rb
+++ config/initializers/cache.rb
@@ -1,7 +1,7 @@
 
-options = { :namespace => "fest.#{Rails.env}" }
+options = { :namespace => "fest.#{Rails.env}.#{SOURCE_REVISION_NUMBER}" }
 options[:readonly] = true if Rails.env == "test"
 CACHE = MemCache.new('localhost:1211', options)
 
 CachedModel.use_local_cache = true
-CachedModel.ttl = 86400
+CachedModel.ttl = 4.hours
