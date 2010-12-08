e06ae3bcf49caf10fcb7ae24e9e697c0894206de
Bryan Stearns <bryanstearns@gmail.com>
2010-12-07 16:23:35 -1000

    Newer rails wants :key, not :session_key here

--- config/environment.rb
+++ config/environment.rb
@@ -62,7 +62,7 @@ Rails::Initializer.run do |config|
   # Make sure the secret is at least 30 characters and all random, 
   # no regular words or you'll be exposed to dictionary attacks.
   config.action_controller.session = {
-    :session_key => '_fest_session',
+    :key => '_fest_session',
     :secret      => 'SESSION_SECRET_WAS_HERE'
   }
 
