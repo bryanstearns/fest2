d902690e4b00935f83f38201bbd1b92da3fd93f5
Bryan Stearns <bryanstearns@gmail.com>
2011-01-09 10:40:56 -0800

    Don't show the "logged in successfully" flash message

--- app/controllers/session_controller.rb
+++ app/controllers/session_controller.rb
@@ -15,7 +15,7 @@ class SessionController < ApplicationController
         cookies[:auth_token] = { :value => self.current_user.remember_token , :expires => self.current_user.remember_token_expires_at }
       end
       redirect_back_or_default('/')
-      flash[:notice] = "Logged in successfully"
+      # flash[:notice] = "Logged in successfully"
     else
       flash[:warning] = "Oops, that's not a recognized email address and password."
       @attempt_failed = true
