d59e0fe8ef42e4802b71fbbe3377342ef33c3c3f
Bryan Stearns <bryanstearns@gmail.com>
2011-02-13 00:18:05 -0800

    Add manual-update mechanism

--- lib/manual_update.rb
+++ lib/manual_update.rb
@@ -3,19 +3,8 @@ module ManualUpdate
   def self.by(login_name)
     require 'ruby-debug'
     ActiveRecord::Base.transaction do
-      ActiveRecord::Base.current_user = User.find_by_login!(login_name)
-      PaperTrail.whodunnit = login_name
-
       puts "Running update..."
       yield self
-
-      puts "Checking database validity..."
-      site = "local" if Rails.env.development?
-      unless Checker.check(:verbose => ENV["VERBOSE"],
-                           :site => site)
-        puts "Check failed... rolling back."
-        raise ActiveRecord::Rollback
-      end
 
       if ENV["DRY_RUN"]
         # note: DRY_RUN=1 must come before "ruby" on the command line!
