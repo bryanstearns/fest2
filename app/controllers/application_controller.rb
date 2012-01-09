37c97bd746acd3239a4f3b85fd9197b478d1098f
Bryan Stearns <bryanstearns@gmail.com>
2012-01-08 17:05:43 -0800

    Simplify layout setup a little
    It seems that we don't need to override the layout when handling formatted
    requests anymore.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -18,11 +18,9 @@ class ApplicationController < ActionController::Base
   # Uncomment the :secret if you're not using the cookie session store
   protect_from_forgery # :secret => '628adf0a9fb7cd0ff7dabfe3c991d940'
   
-  # Use our standard layout for all non-AJAX/non-RSS-feed requests
-  layout proc {|c| (c.request.xhr? || \
-    %w[xml pdf].any? {|fmt| c.request.format.to_s.include?(fmt) } \
-    ) ? false : "standard" }
-  
+  # Use our standard layout
+  layout "standard"
+
   # See ActionController::Base for details 
   # Uncomment this to filter the contents of submitted sensitive data parameters
   # from your application log (in this case, all fields with names like "password"). 
